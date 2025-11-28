6d4a387aa58e0b39640ab49c3ac0e25d20a91be6
Paulo Neves <pnevespi@gmail.com>
2025-11-28 11:23:51 -0300

    Exercicio 17: teste de settings

---
+++ backend/tests/setup/test_portal_settings.py
@@ -0,0 +1,27 @@
+"""Portal settings tests."""
+from plone import api
+
+
+class TestPortalSettings:
+    """Test that Portal configuration is correctly done."""
+
+
+    def test_portal_social_media(self, portal):
+        """Test portal Email."""
+        value = api.portal.get_registry_record("plone.twitter_username")
+        expected = "pnevespi"
+        assert value == expected
+
+
+    def test_portal_email(self, portal):
+        """Test portal Email."""
+        value = api.portal.get_registry_record("plone.email_from_name")
+        expected = "Intranet Tribunal Eleitoral do PI"
+        assert value == expected
+
+
+    def test_portal_title(self, portal):
+        """Test portal title."""
+        value = api.portal.get_registry_record("plone.site_title")
+        expected = "Intranet Tribunal Eleitoral do PI"
+        assert value == expected
