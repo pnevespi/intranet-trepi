39def3910eb022282a4610ce622ccc333aa6022d
Paulo Neves <pnevespi@gmail.com>
2025-12-01 12:45:21 -0300

    Exercicio 19-26 - corrindo erros

--- backend/tests/setup/test_portal_settings.py
+++ backend/tests/setup/test_portal_settings.py
@@ -2,11 +2,27 @@
 
 from plone import api
 
+import pytest
 
 
 class TestPortalSettings:
     """Test portal settings."""
 
+    @pytest.mark.parametrize(
+        "key,expected",
+        [
+            ("plone.smtp_host", "localhost"),
+            ("plone.smtp_port", 25),
+            ("plone.twitter_username", "pnevespi"),
+            ("plone.default_language", "pt-br"),
+            (
+                "plone.available_languages",
+                [
+                    "pt-br",
+                ],
+            ),
+        ],
+    )
     def test_settings(self, portal, key: str, expected: str | int | list[str]):
         """Test that the portal title is set correctly."""
         value = api.portal.get_registry_record(key)
