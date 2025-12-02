1a68196942ca3ccb63b2b21a4d6ef8974b983508
Paulo Neves <pnevespi@gmail.com>
2025-12-02 12:14:03 -0300

    Exercicio 32: feito

--- backend/src/trepi/intranet/content/pessoa.py
+++ backend/src/trepi/intranet/content/pessoa.py
@@ -1,12 +1,20 @@
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from trepi.intranet import _
+from zope import schema
 from zope.interface import implementer
 
 
 class IPessoa(model.Schema):
     """Definição de uma Pessoa."""
 
+    cargo = schema.Choice(
+        title=_("Cargo"),
+        vocabulary="trepi.intranet.vocabulary.cargos",
+        required=False,
+    )
+
 
 @implementer(IPessoa)
 class Pessoa(Container):
-    """Uma Pessoa do TRE-PI."""
+    """Uma Pessoa no TRE-PI."""
